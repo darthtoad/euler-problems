0e44c8dc60f39008155f67fd1d266e2a8b34c1f7
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-03-02 16:40:38 -0800

    can see details but format is subpar

--- src/app/problem-detail/problem-detail.component.ts
+++ src/app/problem-detail/problem-detail.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Problem } from '../problem.model';
+import { ProblemService } from '../problem.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-problem-detail',
   templateUrl: './problem-detail.component.html',
-  styleUrls: ['./problem-detail.component.css']
+  styleUrls: ['./problem-detail.component.css'],
+  providers: [ProblemService]
 })
 export class ProblemDetailComponent implements OnInit {
 
-  constructor() { }
+  problemId: string;
+  problemToDisplay;
+
+  constructor(private route: ActivatedRoute, private location: Location, private problemService: ProblemService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.problemId = urlParameters['id'];
+    })
+    this.problemToDisplay = this.problemService.getProblemById(this.problemId);
   }
 
 }
